2d83c66ee14760e3e799913c7350b5eb4453039e
FrancisNG22 <ngpokman@gmail.com>
2022-04-13 17:15:09 +0800

    second commit third commit

--- myProgram2.py
+++ myProgram2.py
@@ -3,7 +3,12 @@ print('Testing123')
 Fruit = input("Please enter the favorite fruit: ")
 print(Fruit)
 
- x=3
- y=4
- z=x*y
- print(z,x,y)
+x = 2
+y = 4
+z = x*y
+print(z)
+
+x = 3
+y = 4
+z = x ** y
+print(z)
